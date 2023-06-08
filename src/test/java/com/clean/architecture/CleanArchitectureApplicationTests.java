e4b5d70f28b1f8f54d9bfbf346f129e7bc208c18
Rokiatou <diallorokiatou1@gmail.com>
2023-06-08 15:59:16 +0200

    add HelloWordController

--- src/test/java/com/clean/architecture/CleanArchitectureApplicationTests.java
+++ src/test/java/com/clean/architecture/CleanArchitectureApplicationTests.java
@@ -1,4 +1,4 @@
-package com.clean.architecture.helloworld;
+package com.clean.architecture;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ src/test/java/com/clean/architecture/helloword/HelloWordControllerTest.java
@@ -0,0 +1,19 @@
+package com.clean.architecture.helloword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+@SpringBootTest
+class HelloWordControllerTest {
+    @Autowired
+    HelloWordController helloWordController;
+
+    @Test
+    void sayHelloWorld() {
+        helloWordController = new HelloWordController();
+        String result = helloWordController.sayHelloWorld();
+        assertEquals("Hello world", result);
+    }
+}
