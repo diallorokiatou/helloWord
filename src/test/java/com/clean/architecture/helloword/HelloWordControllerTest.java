ae3d769d828993fcd729bd0261eeda8b521dc036
Rokiatou <diallorokiatou1@gmail.com>
2023-06-08 16:33:27 +0200

    refacto HelloWordControllerTest

--- src/test/java/com/clean/architecture/helloword/HelloWordControllerTest.java
+++ src/test/java/com/clean/architecture/helloword/HelloWordControllerTest.java
@@ -2,17 +2,23 @@ package com.clean.architecture.helloword;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HelloWordControllerTest {
+    @Value(value="${local.server.port}")
+    private int port;
+
     @Autowired
-    HelloWordController helloWordController;
+    private TestRestTemplate restTemplate;
 
     @Test
-    void sayHelloWorld() {
-        String result = helloWordController.sayHelloWorld();
+    public void greetingShouldReturnDefaultMessage() {
+        String result = this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class);
         assertEquals("Hello world", result);
     }
 }
