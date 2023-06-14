6363050f0aa368ad226f9500791dd98f1c4a1bd5
Rokiatou <diallorokiatou1@gmail.com>
2023-06-15 00:45:29 +0200

    add entity

---
+++ src/main/java/com/clean/architecture/Greeting/GreetingEntity.java
@@ -0,0 +1,9 @@
+package com.clean.architecture.Greeting;
+
+public class GreetingEntity {
+    private final String message = "Hello word";
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/test/java/com/clean/architecture/Greeting/GreetingEntityTest.java
@@ -0,0 +1,15 @@
+package com.clean.architecture.Greeting;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GreetingEntityTest {
+
+    @Test
+    void uniqueMessage() {
+        GreetingEntity  greetingEntity = new GreetingEntity();
+        String message = greetingEntity.getMessage();
+        assertEquals("Hello word", message);
+    }
+}
